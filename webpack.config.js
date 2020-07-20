98d16ae9171fe398b45f1f9f84b7c571f3ece353
pxCheney <18511293107@163.com>
2020-07-20 17:54:18 +0800

    [feat] 3-2: 管理输出，动态引入bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // 如果此时更改或添加新的入口名称，将会重新生成构建bundle，但是index.html仍会引用旧的名字。此时可以使用HtmlWebpackPlugin来解决这个问题
@@ -9,5 +10,10 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
 };
