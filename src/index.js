02d87f50c59a7eaa31d6da54c245b1a7d97ef252
pxCheney <18511293107@163.com>
2020-07-20 17:37:48 +0800

    [feat] 2-4: revert，回退处理

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import _ from 'lodash';
-import Icon from './icon.png'
-import './style.css';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
-  // lodash 是由当前 script 脚本 import 导入进来的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // 将图像添加到我们现有的 div。
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  console.log(Data)
   return element;
 }
 
