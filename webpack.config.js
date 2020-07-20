9974c4dea6ad65db25efb82dea7e73b3be94658d
pxCheney <18511293107@163.com>
2020-07-20 17:49:54 +0800

    [feat] 3-1: 管理输出，多入口

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  // 如果此时更改或添加新的入口名称，将会重新生成构建bundle，但是index.html仍会引用旧的名字。此时可以使用HtmlWebpackPlugin来解决这个问题
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
