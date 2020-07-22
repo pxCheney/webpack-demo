f87f2284f118ee7f10aeed864c7cc965e418ce4d
pxCheney <18511293107@163.com>
2020-07-22 14:53:25 +0800

    [feat] 5-1: 模块热替换，通过Node.js API 使用

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   // 如果此时更改或添加新的入口名称，将会重新生成构建bundle，但是index.html仍会引用旧的名字。此时可以使用HtmlWebpackPlugin来解决这个问题
+  // mode: 'development',
   devtool: 'inline-source-map',
   entry: {
     app: './src/index.js',
