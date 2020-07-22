b843665f1cb9e217bbf7bc3ba4e7dc75fd8ee7bb
pxCheney <18511293107@163.com>
2020-07-22 15:22:52 +0800

    [feat] 6-1: tree shaking，添加一个通用模块

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
-import _ from 'lodash';
+import { cube } from './math.js';
 import printMe from './print.js';
-import './style.css';
 
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+  var element = document.createElement('pre');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
 
-// document.body.appendChild(component());
-  let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
-  document.body.appendChild(element);
+document.body.appendChild(component());
 
-if (module.hot) {
-  console.log(module)
-  module.hot.accept('./print.js', function(a) {
-    console.log('Accepting the updated printMe module!');
-    // printMe();
-    document.body.removeChild(element);
-    element = component(); // 重新渲染页面后，component 更新 click 事件处理
-    document.body.appendChild(element);
-  })
-}
