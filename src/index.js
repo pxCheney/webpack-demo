b21b3995ec4afe05f4b1e29f19ecbcf96005fdd8
pxCheney <18511293107@163.com>
2020-08-05 16:29:17 +0800

    [feat] 1-2: 通过css loader 加载css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // ./src/index.js
+import './index.css'
 import createHeader from './header.js'
 const heading = createHeader()
 document.body.append(heading)
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,19 @@ const path = require('path')
 module.exports = {
   mode: 'none',
   entry: './src/index.js',
+  // entry: './src/index.css',
   output: {
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // 根据打包过程中所遇到文件路径匹配是否使用这个 loader
+        use: [ // 指定具体的 loader
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 }
